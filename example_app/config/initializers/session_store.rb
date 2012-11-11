84144bfe773afd5f5070cfbdfb580081526806b7
Craig McNamara <craig@caring.com>
2012-11-10 21:26:46 -0800

    Supporting Ruby 1.8.

--- example_app/config/initializers/session_store.rb
+++ example_app/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ExampleApp::Application.config.session_store :cookie_store, key: '_example_app_session'
+ExampleApp::Application.config.session_store :cookie_store, :key => '_example_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
