ddb3e2807c42683b03a5bb71b3b75fd32e4fe173
Craig McNamara <craig@caring.com>
2013-01-30 14:12:43 -0800

    Check for defined. Version bump.

--- lib/sexxy_emails.rb
+++ lib/sexxy_emails.rb
@@ -2,11 +2,11 @@ module SexxyEmails
 
   require File.expand_path('../sexxy_emails/css', __FILE__)
 
-  VERSION = '0.0.3'.freeze
+  VERSION = '0.0.4'.freeze
 
   class << self
     def public_folder
-      if apps = Padrino::Application.descendants
+      if defined?(Padrino::Application) && apps = Padrino::Application.descendants
         apps.first.public_folder
       elsif defined?(Rails)
         Rails.public_path
@@ -14,11 +14,11 @@ module SexxyEmails
     end
 
     def included(base)
-      if base < Padrino::Application
+      if defined?(Padrino::Application) && base < Padrino::Application
         require File.expand_path('../sexxy_emails/padrino_mailer', __FILE__)
         Mail::Message.send(:include, PadrinoMailer::MailerHelper)
         Mail::Message.send(:include, PadrinoMailer::MailerMethods)
-      elsif base < ActionMailer::Base
+      elsif base < ::ActionMailer::Base
         require File.expand_path('../sexxy_emails/action_mailer', __FILE__)
         base.send(:include, ActionMailer::MailerMethods)
         base.instance_eval do
